3a100c0a4296fdc0deb3b0fe21d4b132fdeaef36
samuel-bazinet <59840350+samuel-bazinet@users.noreply.github.com>
2020-01-30 12:35:25 -0500

    Add files via upload

--- rev_file.cpp
+++ rev_file.cpp
@@ -45,11 +45,13 @@ void freadnames(ifstream &f,char *list [])
 	char x[200];
 
 	int i = 0;
-
-	// write a while loop to reads string form the file and put it in x till the end file
-		// inside the loop allocate the dynamic array for list[i]
-		// copy string in x to list[i] array
-		// increment i 
+	
+	while (!f.eof()) {
+		f >> x;
+		list[i] = new char[strlen(x)+1];
+		strcpy(list[i],x);
+		i++;
+	}
 
 	list[i] = nullptr;  // We put the null to the last pointer to mark that the last element in list
 }
@@ -61,7 +63,7 @@ void fwritenames_reverse(ofstream &f,char *list [])
 
 	for(int j = i-1; j >= 0 ; --j)
 	{
-		// your code is here
+		f << list[j] << endl;
 	}
 }
 void freenames(char *list [])
